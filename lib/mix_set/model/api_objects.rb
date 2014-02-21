24de9622fb416c9f210c3554240480d4bf34f8a8
nataliq <nataliyapatsovska@Nataliyas-MacBook-Pro.local>
2014-02-21 16:57:15 +0200

    Change file structure. Add track's artist.

--- lib/mix_set/model/api_objects.rb
+++ lib/mix_set/model/api_objects.rb
@@ -1,73 +1,79 @@
-module ApiCommunication
+module MixSet
+  module ApiCommunication
 
-  class Parser
-    attr_accessor :object_class, :nesting
+    class Parser
+      attr_accessor :object_class, :nesting
 
-    def initialize(object_class: nil, nesting: [])
-      @object_class = "ApiCommunication::ApiObjects::#{object_class}"
-      @nesting = nesting
+      def initialize(object_class: nil, nesting: [])
+        @object_class = "MixSet::ApiCommunication::ApiObjects::#{object_class}" if object_class
+        @nesting = nesting
+      end
+
+      def parse_json(json)
+        return json unless json.class == Hash
+        objects = json.get_nested_object nesting
+        objects = parse_objects_of_class objects, object_class if object_class and objects
+
+        objects
+      end
+
+      private
+
+      def parse_objects_of_class(json, object_class)
+        [json].flatten.map { |object_data| Object.const_get(object_class).new object_data }
+      end
     end
-    
-    def parse_json(json)
-      return json unless json.class == Hash
-      objects = json.get_nested_object nesting
-      objects = parse_objects_of_class objects, object_class if object_class and objects
-      objects
-    end
 
-    private
+    module ApiObjects
 
-    def parse_objects_of_class(json, object_class)
-      [json].flatten.map { |object_data| Object.const_get(object_class).new object_data }
-    end
-  end
+      class Base8TracksObject
+        def initialize(json)
+          mapped_properties(json, self.class.mappings).each do |property, value|
+            self.send("#{property}=", value)
+          end
+        end
 
-  module ApiObjects
+        def mapped_properties(json, mappings)
+         Hash[json.map { |key, value| [(mappings[key] or key), value] }]
+        end
 
-    class Base8TracksObject
-      def initialize(json)
-        mapped_properties(json, self.class.mappings).each do |property, value|
-          self.send("#{property}=", value)
+        def self.mappings; Hash.new; end
+
+        def method_missing(method, *args) ; end  
+      end
+
+
+      class Mix < Base8TracksObject
+        attr_accessor :id, :name, :duration
+
+        def duration=(seconds_string)
+          @duration = Time.at(seconds_string.to_i).gmtime.strftime('%R:%S')
         end
       end
 
-      def mapped_properties(json, mappings)
-       Hash[json.map { |key, value| [(mappings[key] or key), value] }]
+
+      class Track < Base8TracksObject
+        attr_accessor :id, :title, :stream_url, :artist
+
+        def self.mappings
+          {
+            "track_file_stream_url" => "stream_url",
+            "name" => "title",
+            "performer" => "artist"
+          }
+        end
       end
 
-      def self.mappings; Hash.new; end
 
-      def method_missing(method, *args) ; end  
-    end
+      class Tag < Base8TracksObject
+        attr_accessor :name, :count
 
-
-    class Mix < Base8TracksObject
-      attr_accessor :id, :name, :duration
-
-      def duration=(seconds_string)
-        @duration = Time.at(seconds_string.to_i).gmtime.strftime('%R:%S')
-      end
-    end
-
-
-    class Track < Base8TracksObject
-      attr_accessor :id, :name, :stream_url
-
-      def self.mappings
-        {"track_file_stream_url" => "stream_url"}
-      end
-    end
-
-
-    class Tag < Base8TracksObject
-      attr_accessor :name, :count
-
-      def self.mappings
-        {"cool_taggings_count" => "count"}
+        def self.mappings
+          {"cool_taggings_count" => "count"}
+        end
       end
     end
   end
-
 end
 
 class Hash
