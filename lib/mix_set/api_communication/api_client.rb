792d5038c4a841e248ac8a6ba62855d851a6ef36
nataliq <nataliyapatsovska@Nataliyas-MacBook-Pro.local>
2014-02-21 23:13:31 +0200

    Refactor.

--- lib/mix_set/api_communication/api_client.rb
+++ lib/mix_set/api_communication/api_client.rb
@@ -21,10 +21,11 @@ module MixSet
 
       def authorize(username, password)
         params = {login: username, password: password}
-        parser = Parser.new nesting: "user/user_token"
-        @user_token = objects_for_request "/sessions", params,  method: :post, parser: parser
-        if @user_token
-          save_user_data({token: @user_token, username: username})
+        parser = Parser.new nesting: "user"
+        user = objects_for_request "/sessions", params,  method: :post, parser: parser
+        if user
+          @user_token = user["user_token"]
+          save_user_data({token: @user_token, username: username, id: user["id"]})
         end
         @user_token.nil? ? false : username
       end
@@ -38,7 +39,7 @@ module MixSet
       end
 
       def delete_user_data
-        save_user_data({token: nil, username: nil})
+        save_user_data({token: nil, username: nil, id: nil})
       end
     end
 
@@ -60,10 +61,11 @@ module MixSet
     class ApiClient
 
       BASE_URL = 'http://8tracks.com'
-      API_KEY_HEADER = 'X-Api-Key'
-      API_KEY = '86525d0414507857fbbcf1cdad9606fc8e0efc55'
-      API_VERSION_KEY = "api_version"
-      API_VERSION = 3
+      API_KEY = 'X-Api-Key'
+      API_TOKEN_KEY = 'X-User-Token'
+      KEY = '86525d0414507857fbbcf1cdad9606fc8e0efc55'
+      VERSION_KEY = "api_version"
+      VERSION = 3
 
       private_constant :API_KEY
 
@@ -77,7 +79,7 @@ module MixSet
 
       def initialize
         @user_token = retrieve_user_data :token
-        options = {:url => BASE_URL, :headers => {API_KEY_HEADER => API_KEY}, :params => {API_VERSION_KEY => API_VERSION}}
+        options = {:url => BASE_URL, :headers => {API_KEY => KEY}, :params => {VERSION_KEY => VERSION}}
         @connection = Faraday.new(options) do |faraday|
           faraday.request  :url_encoded
           faraday.use ApiMiddleware
@@ -95,7 +97,7 @@ module MixSet
       private 
 
       def objects_for_request(path, params = {}, method: :get, parser: nil)
-        json = @connection.public_send(method, "#{path}.json", params).body
+        json = @connection.public_send(method, "#{path}.json", params, API_TOKEN_KEY => (@user_token or "")).body
         objects = (parser.parse_json json unless parser.nil?) or []
       end
     end
